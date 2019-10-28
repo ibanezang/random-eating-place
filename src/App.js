4467ca2099a68a18eb8afc300665f3e691277f22
Anang Satria <anang.dista.satria@gmail.com>
2019-10-28 21:59:16 +0700

    create random pick control

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import EatingPlaceList from './components/EatingPlaceList';
 import eatingPlaces from './data/eatingPlaces';
+import RandomPickControl from './components/RandomPickControl';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <EatingPlaceList places={eatingPlaces}></EatingPlaceList>
+      <RandomPickControl places={eatingPlaces}/>
     </div>
   );
 }
---
+++ src/components/RandomPickControl.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function RandomPickControl(props){
+    return (
+        <div>
+            <button className="btn btn-primary">Pick Random</button>
+            {}            
+        </div>
+    );
+}
+
+export default RandomPickControl;
