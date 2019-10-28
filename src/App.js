414ae0d595a3ff7036918e53884bcbf0688b1dc1
Anang Satria <anang.dista.satria@gmail.com>
2019-10-29 06:21:39 +0700

    Create logic to pick random place

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import './App.css';
-import EatingPlaceList from './components/EatingPlaceList';
 import eatingPlaces from './data/eatingPlaces';
 import RandomPickControl from './components/RandomPickControl';
 
 function App() {
   return (
     <div className="container">
-      <EatingPlaceList places={eatingPlaces}></EatingPlaceList>
       <RandomPickControl places={eatingPlaces}/>
     </div>
   );
--- src/components/RandomPickControl.js
+++ src/components/RandomPickControl.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-function RandomPickControl(props){
+function RandomPickControl(props) {
+    const places = props.places;
+    const [selected, setSelected] = useState(0);
     return (
         <div>
-            <button className="btn btn-primary">Pick Random</button>
-            {}            
+            <div className="text-center">
+                <h1>{places[selected].name}</h1>
+            </div>
+            <div className="text-center">
+                <button className="btn btn-primary" onClick={() => setSelected(getRandom(places.length))}>Pick Random</button>
+            </div>
         </div>
     );
+}
+
+function getRandom(max) {
+    return Math.floor(Math.random() * max);
 }
 
 export default RandomPickControl;
