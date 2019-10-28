f822ed5e17926bef5ba91960d14361ce8e75afaa
Anang Satria <anang.dista.satria@gmail.com>
2019-10-29 06:52:07 +0700

    Updates

---
+++ precache-manifest.17b0e2249763df1cb22f0c7f416387f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80b3c8bf03d98b20fdd0c0fda7634c9b",
+    "url": "/random-eating-place/index.html"
+  },
+  {
+    "revision": "eba1951693a3d67622b4",
+    "url": "/random-eating-place/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "1e5687eab2750ef9edef",
+    "url": "/random-eating-place/static/css/main.5a3086f6.chunk.css"
+  },
+  {
+    "revision": "eba1951693a3d67622b4",
+    "url": "/random-eating-place/static/js/2.3e793ab0.chunk.js"
+  },
+  {
+    "revision": "1e5687eab2750ef9edef",
+    "url": "/random-eating-place/static/js/main.eda58869.chunk.js"
+  },
+  {
+    "revision": "8dd8e264822746fdd1cd",
+    "url": "/random-eating-place/static/js/runtime-main.0b505a83.js"
+  }
+]);
