c7b1ff1da8a799166ce0102916f8f4db501585f1
Anang Satria <anang.dista.satria@gmail.com>
2019-10-29 06:31:57 +0700

    update style

--- src/components/RandomPickControl.js
+++ src/components/RandomPickControl.js
@@ -2,11 +2,11 @@ import React, {useState} from 'react';
 
 function RandomPickControl(props) {
     const places = props.places;
-    const [selected, setSelected] = useState(0);
+    const [selected, setSelected] = useState(-1);
     return (
         <div>
-            <div className="text-center">
-                <h1>{places[selected].name}</h1>
+            <div className="random-display text-center">
+                <h1>{selected === -1 ? "Please tap on Pick Random Button" : places[selected].name}</h1>
             </div>
             <div className="text-center">
                 <button className="btn btn-primary" onClick={() => setSelected(getRandom(places.length))}>Pick Random</button>
